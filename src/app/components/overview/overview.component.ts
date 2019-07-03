0d08ab736b8ab81930211821289c1f0b852a0c35
GCebria <gerardcebria@gmail.com>
2019-07-03 14:18:29 +0200

    Assignment 6 - Filter

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -13,14 +13,33 @@ import { DataService } from 'app/services/data';
 export class OverviewComponent implements OnInit {
 
   $items: Subject<DataItem[]> = new BehaviorSubject<DataItem[]>([]);
-  arrayShow : Array<Boolean> = [];
+  /*Auxiliar array for the option button*/
+  arrayShow: Array<Boolean> = [];
+  
+  /*Vars for the filter system*/
+  filterList = []
+  filterSelect: string = "Name";
+  filterValue: string = "";
+  
   constructor(
     private dataService: DataService,
     private router: Router,
     private route: ActivatedRoute,
-  ) {}
+  ) { }
 
   ngOnInit() {
+    this.loadItemList();
+  }
+
+  onSelectItem(id: number) {
+    this.router.navigate(['/items', 'details', id]);
+  }
+
+  onNewItem() {
+    this.router.navigate(['new']);
+  }
+
+  loadItemList(){
     this.route.params.subscribe(params => {
       this.dataService.listItems().subscribe(items => {
         this.$items.next(items);
@@ -28,11 +47,44 @@ export class OverviewComponent implements OnInit {
     });
   }
 
-  onSelectItem(id: number) {
-    this.router.navigate(['/items','details', id]);
+  searchByFilter(){
+    var newFilter = {
+      filter: this.filterSelect,
+      value: this.filterValue
+    } 
+    this.filterList.push(newFilter);
   }
 
-  onNewItem() {
-    this.router.navigate(['new']);
+  deleteFilter(filter: any) {
+    let index = 0;
+    this.filterList.forEach(element => {
+      if (filter.value === element.value &&
+        filter.filter === element.filter) {
+        console.log(filter + index);
+        this.filterList.splice(index, 1);
+      }
+      index++;
+    });
   }
+
+  deleteItem(idValue: number){
+    var idNumber: number = +idValue;
+    this.dataService.getItem(idNumber).subscribe(item => {
+      this.dataService.deleteItem(item);
+      this.loadItemList();
+    });
+  }
+
+  /*
+  searchByName(name: string){
+    this.route.params.subscribe(params => {
+      this.dataService.listItems().subscribe(items => {
+        if (item.includes(name)) { 
+          
+          this.$items.next(items);
+        }
+      });
+    });
+  }*/
+
 }
